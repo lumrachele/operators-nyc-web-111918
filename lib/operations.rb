a0e53aadfb11c075228e463bb254a3b639c66f20
lumrachele <rlum894@gmail.com>
2018-11-11 19:19:45 -0500

    updated operations.rb with conditional and ternary operator methods. updated spec file to require operations.rb

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,14 @@
+require 'pry'
+
 def unsafe?(speed)
-
+	speed = "#{speed}".to_i
+	if speed < 40 || speed > 60
+		true
+	else
+		false
+	end
 end
-
-
 
 def not_safe?(speed)
-	
+	speed < 40 || speed > 60? true:false
 end
-	
-
-
